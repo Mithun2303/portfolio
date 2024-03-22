1852ffdede7dec4cc778349f6e8fba9895962809
Mithun@2303 <mithunkarthick1610@gmail.com>
2024-03-22 13:19:57 +0530

    CA

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { LandingPage } from './LandingPage'
+import {S} from "./crc";
+import {H} from "./h";
 function App() {
 
   return(
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<LandingPage/>}></Route>
+      <Route path="/crc" element={<S/>}/>
+      <Route path="/h" element={<H/>}/>
+
       </Routes>
       </BrowserRouter>
   )
