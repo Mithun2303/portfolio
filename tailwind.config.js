b5fbc8e7a136a36a232491396f8a0e49e87098a6
Mithun Venkatesan <84895703+Mithun2303@users.noreply.github.com>
2024-07-01 10:13:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,13 @@ export default {
       },
       dropShadow: {
         '3xl': '8px 8px 4px #1DB954',
+        '2xl': '2px 2px 4px #1DB954',
       },
       colors:{
         primary:"#1DB954",
       },
       transitionProperty:{
+        
       }
     },
   },
